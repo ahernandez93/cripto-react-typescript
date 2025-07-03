9c3229df63a199ed0f25928f894f2ff089677be4
Allan <allan_hc@outlook.com>
2025-07-03 17:53:58 -0600

    Inicializacion

---
+++ src/components/CriptoSearchForm.tsx
@@ -0,0 +1,32 @@
+import { currencies } from "../data"
+
+export default function CriptoSearchForm() {
+    return (
+        <form className="form">
+            <div className="field">
+                <label htmlFor="currency">Moneda:</label>
+                <select
+                    name="currency"
+                    id="currency">
+                    <option value="">-- Seleccionar --</option>
+                    {currencies.map(currency => (
+                        <option key={currency.code} value={currency.code}>
+                            {currency.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="field">
+                <label htmlFor="criptocurrency">Criptomonedas:</label>
+                <select
+                    name="criptocurrency"
+                    id="criptocurrency">
+                    <option value="">-- Seleccionar --</option>
+                </select>
+            </div>
+
+            <input type="submit" value="Cotizar" />
+        </form>
+    )
+}
